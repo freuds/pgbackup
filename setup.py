84bf4df82aa1076e6f676ce138bed64189926f44
Freuds <frederic.willien@gmail.com>
2021-03-03 17:06:18 +0100

    add requires packages + console_scripts endpoints

--- setup.py
+++ setup.py
@@ -13,5 +13,10 @@ setup(
     author_email='fred@freuds.me',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=[]
+    install_requires=['boto3'],
+        entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ],
+    }
 )
